0d109ef4141cfe9d92c0ffbb932e21be1e39f1e7
eshapatel2005 <esha.patel5605@gmail.com>
2026-07-01 15:45:13 +0530

    Create a file

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-console.log("Hello");
+// import fs from 'fs';
 
-//Addition of 2 numbers
-let a=5;
-let b=10;
-console.log("Addition : ",a+b); 
+// console.log("Hello");
+
+// //Addition of 2 numbers
+// let a=5;
+// let b=10;
+// console.log("Addition : ",a+b); 
+
+
+// //File System Module
+// const fs=require('fs');
+
+
+//Create a file
+const fs = require("fs");
+fs.writeFile("demo.txt", "Hello Node.js ...", (err) => {
+    if (err) throw err;
+
+    console.log("File created successfully");
+});
+
+
+
+// fs.readFile("demo.txt", "utf8", (err, data) => {
+//     if (err) throw err;
+
+//     console.log(data);
+// });
+
+// console.log("Program End");
